8a0bc2fc1f5f2062adecfd5340f7d24cfcb4167d
kanobt61@gmail.com <kanobt61@gmail.com>
2013-04-10 18:20:16 -0400

    Woot woot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zeratul::Application.config.secret_token = '52a4d742a2dab0df3c040a5140402b67e08515eb512de1c8e06d7438acdd50d000e91fe29df225e7b9963c094abcf0dd331ec78b7ef0bfc9429ef3048931cb60'
